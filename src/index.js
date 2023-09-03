ebee7d2e7e06a6d3042c77a74fa61ba9afe53c9d
Velorian <TerleckyDima92@gmail.com>
2023-09-03 16:27:57 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const root = createRoot(container);
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter basename='/goit-react-hw-06-phonebook'>
+            <BrowserRouter basename='/goit-react-hw-07-phonebook'>
                 <App />
             </BrowserRouter>
         </Provider>
